891347126bd17bd83f2c2154133916fb004e6bc1
Harvey Lallave <harvey_lallave@gmail.com>
2018-07-23 19:03:52 +0800

    Vector Addition & Scaling

--- src/Driver.java
+++ src/Driver.java
@@ -31,9 +31,10 @@ public class Driver {
 
 	        while((line = bufferedReader.readLine()) != null) {
         		Vector v = new Vector();
-        		String[] xyz = line.split(" ");
-	        	for(int i = 0; i < xyz.length; i++){
-	        		v.setCoordinates(i, Integer.parseInt(xyz[i]));
+        		String[] dimensionXYZ = line.split(" ");
+        		int dimension = Integer.parseInt(dimensionXYZ[0]);
+	        	for(int i = 1; i < dimensionXYZ.length; i++){
+	        		v.setCoordinates(i, Integer.parseInt(dimensionXYZ[i]), dimension);
 	        	}
 	        	vectors.add(v);
 	        }   
@@ -70,17 +71,17 @@ public class Driver {
 		Scanner sc = new Scanner(System.in);
 		while (opt > listV.size() || opt < 0){
 			System.out.println("\n");
-			for(int i = 1; i < listV.size(); i++)
-				System.out.println(i + " - " + listV.get(i - 1).toString() + "\n");
+			for(int i = 1; i <= listV.size(); i++)
+				System.out.println(i + " - " + listV.get(i - 1).toString());
 			System.out.print("0 - New Vector\nOption: ");
 			opt = sc.nextInt();
 		}
 		
 		if(opt != 0 )
 			return listV.get(opt - 1);
-		return newVector();	
+		return newVector(listV);	
 	}
-	public static Vector newVector(){
+	public static Vector newVector(ArrayList<Vector> listV){
 		int opt = -1;
 		Scanner sc = new Scanner(System.in);
 		System.out.print("\n\nThe dimention of the vector (1 - 3): ");
@@ -107,12 +108,13 @@ public class Driver {
 				v.setX(sc.nextInt());
 		    }
 	    }
+		listV.add(v);
 		return v;
 	}
 	public static void main(String[] args) {
-		ArrayList<Vector> vectors = new ArrayList<>();
+		ArrayList<Vector> listV = new ArrayList<>();
 		Scanner sc = new Scanner(System.in);
-		Vector v = new Vector(1, 2, 0);
+		Vector v, v2;
 		
 		int opt = -1;
 		
@@ -130,16 +132,31 @@ public class Driver {
 			sc.nextLine();
 			switch(opt){
 				case 1: //VectorAddition
+						// TODO: Multiple vector addition?
+						v = pickVector(listV);
+						v2 = pickVector(listV);
+						if(v.getDimension() != v2.getDimension())
+							System.out.println("Error: Vectors of different dimension cannot be added.");
+						else {
+							System.out.print(v.toString() + " + " + v2.toString() + "\n = ");
+							v.addition(v2);
+							System.out.println(v.toString());
+						}
 						break;
 				case 2: //VectorScaling
+						v = pickVector(listV);
+						System.out.print("Scale factor: ");
+						v.scale(sc.nextInt());
+						System.out.println("New value: " + v.toString());
 						break;
 				case 3: //GaussJordanElimination (with display vector)
 						break;
-				case 4: newVector();
+				case 4: newVector(listV);
 						break;
 				case 5: System.out.print("\nFile name: ");
-						loadVectors(vectors, sc.nextLine());
+						loadVectors(listV, sc.nextLine());
 						break;
+				case 6: listV = new ArrayList<Vector>();
 				case 0: break;
 				default: System.out.println("\nError: Invalid input.\n\n");
 			}
--- src/model/Vector.java
+++ src/model/Vector.java
@@ -2,18 +2,18 @@ package model;
 
 public class Vector {
 
-	private int x,
-				x2,
-				y,
-				z;
+	private Integer x,
+					x2,
+					y,
+					z;
 
 	public Vector(){
 		super();
 
-		x = (Integer) null;
-		x2 = (Integer) null;
-		y = (Integer) null;
-		z = (Integer) null;
+		x  = null;
+		x2 = null;
+		y  = null;
+		z  = null;
 	}
 	
 	public Vector(int x, int y, int z) {
@@ -25,14 +25,24 @@ public class Vector {
 
 	public void scale(int num){
 		// TODO: scale variables
-		this.x *= num;
-		this.y *= num;
-		this.z *= num;
+		
+		switch(getDimension()){
+			case 3: this.z  *= num;
+			case 2: this.y  *= num;
+			case 1: this.x  *= num;
+					if(getDimension() == 1)
+						this.x2 *= num;
+		}
 	};
 	
 	public void addition(Vector v){
-		// TODO: vector addition
-		
+
+		this.x += v.x;		
+		switch(getDimension()){
+			case 3: this.z   += v.z;
+			case 2: this.y   += v.y; break;
+			case 1: this.x2  += v.x2;
+		}
 	};
 
 	public int getX() {
@@ -67,21 +77,35 @@ public class Vector {
 		this.z = z;
 	}
 
-	@Override
-	public String toString() {
-		return "Vector [x=" + x + ", y=" + y + ", z=" + z + "]";
-	}
 
-	public void setCoordinates(int i, int val) {
+	public void setCoordinates(int i, int val, int dimension) {
 
 		switch(i){
 			case 1: x = val; break;
-			case 2: y = val; break;
+			case 2: if(dimension == 1)
+						 x2 = val; 
+					else y = val; 
+					break;
 			case 3: z = val; break;
 			default: System.out.println("Error: Invalid input set coordinates");
 		}
 		
 	}
 	
+	public int getDimension(){
+		if(x2 != (Integer) null)
+			 return 1;
+		else if(z == (Integer)null)
+			 return 2;
+		else return 3;
+	}
+
+	@Override
+	public String toString() {
+		int dimension = getDimension();
+		return dimension + "D Vector [x = " + x + ", " + 
+			  (dimension == 1 ? "x2 = " + x2 : "y  = " + y) + 
+			  (dimension == 3 ? ", z =" + z : "") + "]";
+	}
 	
 }
